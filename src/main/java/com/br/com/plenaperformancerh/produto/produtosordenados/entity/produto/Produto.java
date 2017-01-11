43d8b81da8ce7d35239abbdce6584fbc19d52fea
Livre programacao ltda-me <livre.programacao@gmail.com>
2017-01-10 22:43:19 -0200

    Update ProdutoController

--- src/main/java/com/br/com/plenaperformancerh/produto/produtosordenados/entity/produto/Produto.java
+++ src/main/java/com/br/com/plenaperformancerh/produto/produtosordenados/entity/produto/Produto.java
@@ -2,6 +2,7 @@ package com.br.com.plenaperformancerh.produto.produtosordenados.entity.produto;
 
 import com.br.com.plenaperformancerh.produto.produtosordenados.entity.unidade.Unidade;
 import java.util.Date;
+import java.util.Objects;
 
 /**
  *
@@ -19,5 +20,71 @@ public class Produto {
         this.nome = _produtoNome;
         this.ordenacao = _ordenacao;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 67 * hash + Objects.hashCode(this.nome);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Produto other = (Produto) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" + "data=" + data + ", nome=" + nome + ", ordenacao=" + ordenacao + ", quantidade=" + quantidade + '}';
+    }
+    
+    
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getOrdenacao() {
+        return ordenacao;
+    }
+
+    public void setOrdenacao(String ordenacao) {
+        this.ordenacao = ordenacao;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+    
+    
     
 }
--- src/main/java/com/br/com/plenaperformancerh/produto/produtosordenados/produto/controller/ProdutoController.java
+++ src/main/java/com/br/com/plenaperformancerh/produto/produtosordenados/produto/controller/ProdutoController.java
@@ -3,6 +3,7 @@ package com.br.com.plenaperformancerh.produto.produtosordenados.produto.controll
 import com.br.com.plenaperformancerh.produto.produtosordenados.entity.produto.Produto;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 /**
  *
@@ -10,18 +11,28 @@ import java.util.List;
  */
 public class ProdutoController {
 
-    private List<Produto> produtos = new ArrayList<Produto>();
+    private List<Produto> produtosCollection = new ArrayList<Produto>();
     
-    public ProdutoController(String[] produtos) {
-        parseParameter(produtos);
+    public ProdutoController(String[] args) {
+        parseParameter(args);
     }
 
     public void listByData() {
         System.out.println("\n\n<<< ---- Lista ordenada de produtos por data --- >>>.");
+        
+        for(Produto p : produtosCollection) {
+            //System.out.format("\nData %s Produto %s Ordenação %s", p.getData(), p.getNome(), p.getOrdenacao());
+            System.out.println(p.getNome());
+        }
     }
 
     public void listByProdutoNome() {
         System.out.println("\n\n<<< ---- Lista ordenada de produtos por nome do produto --- >>>.");
+
+        for(Produto p : produtosCollection) {
+            //System.out.format("\nData %s Produto %s Ordenação %s", p.getData(), p.getNome(), p.getOrdenacao());
+            System.out.println(p.getNome());
+        }
     }
 
     private void parseParameter(String[] parametros) {
@@ -38,8 +49,12 @@ public class ProdutoController {
             
             produto = new Produto(data, nome, ordenacao);
             
-            produtos.add(produto);
+            //System.out.println(produto);
+            
+            produtosCollection.add(produto);
         }
+        
+        //System.out.println(produtosCollection.size());
     }
     private static final String DELIMITER = ":";
 
